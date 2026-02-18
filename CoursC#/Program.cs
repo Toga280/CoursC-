9c9d50c74b7e3c5ef987fb6af6fce8339b1dacc2
IPI <IPI@DESKTOP-F0OF1QV>
2026-02-18 13:27:16 +0100

    Ajoutez des fichiers projet.

---
+++ CoursC#/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Rentre ton calcul :");
+string response = Console.ReadLine();
+
+char[] ops = ['+', '-', '*', '%'];
+int i = response.IndexOfAny(ops);
+
+int number1 = int.Parse(response.Substring(0, i));
+int number2 = int.Parse(response.Substring(i + 1));
+char operatorCalcul = response[i];
+
+int result = operatorCalcul switch
+{
+    '+' => number1 + number2,
+    '-' => number1 - number2,
+    '*' => number1 * number2,
+    '%' => number1 % number2
+};
+
+Console.WriteLine($"result : {result}");
